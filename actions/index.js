0fb1b6bdf181350d2ac21b88be236dfb25cdea61
Jennifer Partida <jpartida@baxterplanning.com>
2021-08-16 23:34:03 -0500

    Add ADD_QUESTION to actions

--- actions/index.js
+++ actions/index.js
@@ -2,6 +2,7 @@ export const RECEIVE_DECKS = 'RECEIVE_DECKS'
 export const ADD_DECK = 'ADD_DECK'
 export const DELETE_DECK = 'DELETE_DECK'
 export const ADD_CARD = 'ADD_CARD'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function receiveDecks(decks) {
   return {
